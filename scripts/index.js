f176ef9ac6cd0ce8d18f3d3755abd61d1c66e11c
AndersonSantosFernandes <andersantfer@gmail.com>
2024-03-20 19:40:52 -0300

    Primeira entrega

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,100 @@
-function girarCadastro(){
-    rodado = document.getElementById("formInit")
-    form1 = document.getElementById("init1")
-    btn1 = document.getElementById("btn1")
-    form2 = document.getElementById("init2")
-    btn2 = document.getElementById("btn2")
-    //  = document.getElementById("")
-     
+function logar() {
 
-    form2.style.display='block';
-    form1.style.display='none';
-    rodado.style.transform=' perspective(600px) rotatey(900deg)';
-    // rodado.style.backgroundColor ='purple';
-    form1.style.opacity= '0';
-    form2.style.opacity= '1';
-    btn1.style.opacity= '0';
-    btn2.style.opacity= '1';
+  const retornoLogado = JSON.parse(localStorage.getItem('novos'))
+  console.log(retornoLogado[0]['emailUsuario'])
+
+  let emailUsuario = document.getElementById("email").value
+  let senhaUsuario = document.getElementById("password").value
+
+  //Verificação no "BD" se existe usuario e senha compa´tivel
+
+console.log("Retorno logado",retornoLogado)
+
+
+  if (emailUsuario == retornoLogado[0]['emaiUsuario'] && senhaUsuario == retornoLogado[0]['senhaUsuario']) {
+    sessionStorage.setItem('usuarioLogado', retornoLogado[0]['nomeUsuario'])
+
+    alert("Logado com sucesso")
+    location.href = "./pages/home.html"
+  } else {
+    alert("E-mail ou senha incorretos ou usuário não cadastrado")
   }
+}
+let showUsuario = document.getElementById('nomeUsuario')
+let usando = sessionStorage.getItem('usuarioLogado')
+if (sessionStorage.getItem('usuarioLogado') != null) {
+  showUsuario.innerHTML = `Bem vindo ${usando}`
+}
 
-  function girarLogin(){
-    rodado = document.getElementById("formInit")
-    form1 = document.getElementById("init1")
-    btn1 = document.getElementById("btn1")
-    form2 = document.getElementById("init2")
-    //  = document.getElementById("")
-    //  = document.getElementById("")
-     
 
-    form2.style.display='none';
-    form1.style.display='block';
-    rodado.style.transform=' perspective(600px) rotatey(0deg)';
-    // rodado.style.backgroundColor ='aqua';
-    form1.style.opacity= '1';
-    form2.style.opacity= '0';
-    btn1.style.opacity= '1';
-    btn2.style.opacity= '0';
 
-  }
 
-  function logar(){
-    let emailUsuario = document.getElementById("email")
-    let senhaUsuario = document.getElementById("password")
-    console.log(emailUsuario.value, senhaUsuario.value)
-  }
-  function cadastrar(){
-    let nomeUsuario = document.getElementById("userCadastro")
-    let emailUsuario = document.getElementById("emailCadastro")
-    let senhaUsuario = document.getElementById("passwordCadastro")
-    let confSenha = document.getElementById("confPasswordCadastro")
+function cadastrar() {
 
-    if(senhaUsuario.value === confSenha.value){
-        console.log(nomeUsuario.value,emailUsuario.value,senhaUsuario.value,confSenha.value)
-    
-    }else{
-        alert("Senhas diferentes! Tente novamente")
+  let nomeUsuario = prompt('digite seu nome')
+  let emailUsuario = prompt('digite seu e-mail')
+  let senhaUsuario = prompt('digite uma senha')
+  let confSenha = prompt('confirme a senha')
+  
+
+  const arrayUsuarios = []
+
+  // Cadastra se senhas forem iguais  
+  if (senhaUsuario === confSenha) {
+
+    class NovoUsuario {
+
+      constructor(nome, email, senha) {
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+      }
+
+      cadastrarNovoUsuario() {
+        arrayUsuarios.push({ nome: this.nome, email: this.email, senha: this.senha })
+        localStorage.setItem("userCadastrado", JSON.stringify(arrayUsuarios))
+
+        alert(`${this.nome} você foi cadastrado com sucesso`)
+        girarLogin()
+
+      }
+
     }
+    const criarUsuario = new NovoUsuario(nomeUsuario, emailUsuario, senhaUsuario)
 
-    
+    criarUsuario.cadastrarNovoUsuario()
+
+    // console.log(arrayUsuarios)
+
+  } else {
+    alert("Senhas diferentes! Tente novamente")
   }
+
+}
+
+// Função que encerra a sessão
+const encerrarSessao = () => {
+  sessionStorage.clear('usuarioLogado')
+  location.href = "../index.html"
+}
+
+const listarUsuarios = JSON.parse(localStorage.getItem('novos'))
+
+let corpoTabela = document.getElementById("corpoTabela")
+
+for (let i = 0; i < listarUsuarios.length; i++) {
+
+  console.log(listarUsuarios[i])
+
+  corpoTabela.innerHTML += `
+  
+    <tr>
+      <td>${listarUsuarios[i]['nomeUsuario']}</td>
+      <td>${listarUsuarios[i]['emaiUsuario']}</td>
+      <td>${listarUsuarios[i]['id']}</td>
+      <td>${listarUsuarios[i]['id']}</td>
+    </tr>
+    
+    `
+
+}
