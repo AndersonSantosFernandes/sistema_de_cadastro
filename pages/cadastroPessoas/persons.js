cd23c7f20daebacacef83e3e6bc5f16c54883caa
AndersonSantosFernandes <andersantfer@gmail.com>
2024-04-03 18:55:53 -0300

    Edição de cargo

--- pages/cadastroPessoas/persons.js
+++ pages/cadastroPessoas/persons.js
@@ -49,38 +49,60 @@ function newCargo() {
 }
 
 
-helpArrayUpdate = [] 
-
-function updateOcupation(indiceOcupation){
-
-    alert(indiceOcupation)
-    // let updateOcupation = document.getElementById('newOcupation')
-
-    helpArrayUpdate = JSON.parse(localStorage.getItem('ocupations'))
-
-    // for (let u = 0; u < helpArrayUpdate.length; u++) {
-       
-    //     if(indiceOcupation == u){
-    //         console.log(helpArrayUpdate[u])
-    //         alert('editar com o id ', indiceOcupation)
-    //     }
-      
-    // }
-
-    // helpArrayUpdate  = updateOcupation
-    // localStorage.setItem('ocupations', JSON.stringify(helpArrayUpdate))
-
-
-}
 
 
 // Lista de cargos
 const ocupationList = JSON.parse(localStorage.getItem('ocupations'))
-
+let updateCargos = document.getElementById('updateCargo')
 for (let j = 0; j < ocupationList.length; j++) {
+    //Lista para cadastrar
     listaCargos.innerHTML +=
         `
     <option onclick="updateOcupation(${j})" value="${ocupationList[j]['ocupation']}">${ocupationList[j]['ocupation']}</option>  
       
     `
+
+    //Lista para editar que aparece no segundo modal
+
+    //O botão neste trecho envia o número de índice para a função "updateOcupation()"
+    updateCargos.innerHTML += 
+    `
+    <tr>
+        <td>${ocupationList[j]['ocupation']}</td>
+        <td><input type="text" name="nameUpdate"   style="height:25px;" placeholder="Digite aqui "></td>
+        <td><button onclick="updateOcupation('${j}')" class="btn btn-primary">Salvar</button></td>
+    </tr>
+    
+    `
+    
+}
+
+
+helpArrayUpdate = []
+                            //Valor do índice vem do botão salvar
+function updateOcupation(indiceOcupation) {
+let nameUpdates = document.getElementsByName('nameUpdate')
+
+helpArrayUpdate = ocupationList //Recebe o objeto completo 
+
+    
+    for (let u = 0; u < ocupationList.length; u++) {
+
+        //Procura o objeto com o mesmo numero de ídice
+        if (u == indiceOcupation) {
+
+            //Altera o valor da chave do ídice encontrad 
+            helpArrayUpdate[u]['ocupation'] = nameUpdates[u].value
+            console.log(ocupationList[u]['ocupation'])
+
+            // Retorna o objeto atualizado para o localStorage
+            localStorage.setItem('ocupations' ,JSON.stringify(helpArrayUpdate))
+
+            // Dá um refresh na página para mostrar as atualizações
+            location.href = "cadastrarPessoas.html"
+            break;
+
+        }
+
+    }
 }
